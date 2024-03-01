20e7fd2ea99d0f83c91294fb4a61030b9ded9176
jeylanab <jeylantesi@gmail.com>
2024-03-01 17:14:43 +0530

    navbar for mobile responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,8 +12,8 @@ const Navbar = () => {
 
       <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
         {navLinks.map((nav, index) => (
-          <li key={nav.id} className={'font-poppins px-6 font-normal cursor-pointer text-[16px]  ${index === navLinks.length - 1 ? "mr-0" : "mr-10"} text-white'}>
-            <a href={'${nav.id}'}>
+          <li key={nav.id} className={'font-poppins mr-10 font-normal cursor-pointer text-[16px]  ${index === navLinks.length - 1 ? "mr-0" : "mr-10"} text-white'}>
+            <a href={'#${nav.id}'}>
               {nav.title}
             </a>
           </li>
