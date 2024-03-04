32a0317ce6d03ddc93dab5ad5ed7f937aa027e2d
jeylanab <jeylantesi@gmail.com>
2024-03-04 23:00:05 +0530

    tailwind style for hero section added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import styles from '../style'
+import { discount , robot } from '../assets/assets'
 
-const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+const Hero = () => (
+  <section id='home' className=' flex md:flex-row flex-col ${styles.paddingY}'>
+      <div className={'flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6'}>
+        <div className="flex-row  items-center py-[6px]  px-4 bg-disc">
+          
+        </div>
+      </div>
+  </section>
+)
 
 export default Hero
