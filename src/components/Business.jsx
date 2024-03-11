459d86cc925c92279868147e153ac0e025504b16
jeylanab <jeylantesi@gmail.com>
2024-03-11 17:01:37 +0000

    added bussines component

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { features } from '../constants';
+import styles , { layout } from '../style';
+import Button from './Button';
+
+
 
 const Business = () => {
   return (
-    <div>Business</div>
+    <section id='features' className={layout.section}>
+          <div className={layout.sectionInfo}>
+              <h2 className={styles.heading2}>You will do the business, <br className='sm:block hidden' />
+                 we will handle the Money</h2>
+
+              <p className={`${styles.paragraph} max-w-[470px] my-5`}>  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est
+                 optio laboriosam rerum dolorum eveniet atque perferendis
+                  possimus ipsa repellendus odio? Possimus ut exercitationem culpa porro.
+                 Repudiandae eaque praesentium repellat quas
+              </p>
+          </div>
+    </section>
   )
 }
 
